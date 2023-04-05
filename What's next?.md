42a652a7253cf1fd1d43ce8dfaab1ff431673d75
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-04-04 20:58:18 -0400

    updated reading notes

---
+++ What's next?.md
@@ -0,0 +1,6 @@
+Add console.log() messages within your app to notify the user if they are correct. Before submitting, comment out (don’t delete) your console.log() messages and replace them with the alerts() to complete the project.
+
+Display the users name back to them.
+
+Create a prompt stating whether or not the user is right or wrong.
+
--- js/app.js
+++ js/app.js
@@ -1,16 +1,40 @@
 'use strict'
 
-let warHammer40k = prompt ('Do I like WarHammer40k? Yes or No?')
-if (warHammer40k === prompt 'yes' || warHammer40k === 'y') {
-    alert ('You are right; I do like WarHammer40K');
-}   else if (warHammer40k === 'no')
-console.log
+// let welcome = alert ('Welcome to Jareds about me!')
 
-let japan = prompt ('Do I like japan? Yes or No?')
-if (japan === prompt 'yes' || japan === 'y') {
-    alert ('You are right; I do like WarHammer40K');
-}   else if (japan === 'no')
+// let greeting = prompt('You remind me of someone special, whats your name?')
+// if (greeting === 'yes' || greeting === 'y')
+//     alert('That's a lovely name, thank you for telling me');
 
+// let marine = prompt('Did you know that I worked for the USMC? Yes or No').toLowerCase();
+// if (marine === 'yes' || marine === 'y');
+// alert('I actually worked there for 5 years')
+// // console.log(marine);
+
+// let japan = prompt('Did you know lived in japan? Yes or No');
+// if (japan === 'yes' || japan === 'y');
+// alert('I actually got to shake hands with a yakuza member there!');
+// // console.log(japan);
+
+// let mma = prompt('Did you know that I was involved in Martial arts? Yes or No')
+// if (mma === 'yes' || mma === 'y')
+//     alert('I was an instructor actually!');
+// // console.log(mma);
+
+// let ocSpray = prompt('Did you know that I was sprayed in the eyes with peppers?')
+// if (ocSpray === 'yes' || ocSpray === 'y')
+//     alert('It felt like the longest 2 hours of my life!');
+// // console.log(ocSpray);
+
+
+// console.log(mma);
+
+
+
+// let japan = prompt ('Do I like japan? Yes or No?')
+// if (japan === 'yes' || japan === 'y') {
+//     alert ('You are right; I do like WarHammer40K');
+// }   
 
 
 
