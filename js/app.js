f9b222564ed41b076cff30302043467348b24a0d
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-04-09 13:44:00 -0400

    Fixed\ question 6

--- js/app.js
+++ js/app.js
@@ -5,12 +5,13 @@
 // Add console.log() messages within your app to notify the user if they are correct. Before submitting, comment out (don’t delete) your console.log() messages and replace them with the alerts() to complete the project. 
 
 
-// let welcome = alert('Welcome to my page! Where youll learn all about me!');
+// let welcome = alert('Welcome to my page!');
 
 // // Question 1
 
 // function greeting() {
-//     let greeting = prompt('You remind me of someone special, whats your name?');
+//     let greeting = prompt('You remind me of someone special, whats your name?'); 
+//     if {
 //     alert(greeting + ' is a lovely name, before we move forward I would like to play a game!');
 //     {
 //     } else {
@@ -20,59 +21,76 @@
 // }
 // greeting()
 
-// // Question 2
-// function marine() {
-//     let marine = prompt('Do I look like I could have been in the military? Yes/No').toLowerCase();
-//     if (marine === 'yes' || marine === 'y') {
-//         alert('Youre on a roll! I actually worked there for 5 years!')
-//         // console.log('You're absolutely right! I was in the military!');
-//     } else {
-//         alert('Wow, usually people can tell right away!')
-//     }
-// }
-
-// // Question 3
-// function japan() {
-//     let japan = prompt('Have you ever been to japan? Yes or No');
-//     if (japan === 'yes' || japan === 'y') {
-//         alert('Really? Thats awesome! It was the best experience of my life.');
-//         // console.log(I actually got to shake hands with a yakuza member there);
-//     } else {
-//         alert('I definitely recommend it!')
-//     }
-// }
-
-// // Question 4
-// function mma() {
-//     let mma = prompt('Have you ever trained in MMA? Yes or No')
-//     if (mma === 'yes' || mma === 'y') {
-//         alert('Oh cool, maybe you can teach me something!');
-//         // console.log(mma);
-//     } else {
-//         alert('You should really try it!')
-//     }
-// }
-
-// // Question 5
-// function ocSpray() {
-//     let ocSpray = prompt('Would you believe that I willingly put peppers into my eyes?')
-//     if (ocSpray === 'yes' || ocSpray === 'y') {
-//         alert('It felt like the longest 2 hours of my life!');
-//         // console.log(ocSpray);
-//     } else {
-//         alert('I actually did it 3 times if you can belieive it.')
-//     }
-// }
 
 
 
 
 
+let welcome = alert('Welcome to my page!');
+
+// Question 1
+function greeting() {
+    let greeting = prompt('You remind me of someone special, whats your name?').toLocaleLowerCase();
+    if (greeting === 'no' || greeting === 'n') {
+        // console.log("good job!")
+        alert('Pretty please?')
+    } else {
+        alert(greeting + ' is a lovely name, before we move forward I would like to play a game!');
+    }
+}
+greeting();
+
+// Question 2
+function marine() {
+    let marine = prompt('Do I look like I could have been in the military? Yes/No').toLowerCase();
+    if (marine === 'yes' || marine === 'y') {
+        // console.log("Great job!")
+        alert('Youre on a roll! I actually worked there for 5 years!')
+    } else {
+        alert('Wow, usually people can tell right away!')
+    }
+}
+marine()
 
 
+// Question 3
+function japan() {
+    let japan = prompt('Have you ever been to japan? Yes or No').toLowerCase();
+    if (japan === 'yes' || japan === 'y') {
+        // console.log ("You're good at this!")
+        alert('Really? Thats awesome! It was the best experience of my life.');
+    } else {
+        alert('I definitely recommend it!')
+    }
+}
+japan()
 
 
+// Question 4
+function mma() {
+    let mma = prompt('Have you ever trained in MMA? Yes or No').toLocaleLowerCase();
+    if (mma === 'yes' || mma === 'y') {
+        // console.log("Seems like you already know me.")
+        alert('Oh cool, maybe you can teach me something!');
+        // console.log(mma);
+    } else {
+        alert('If you ever give it try let me know what your thoughts were!')
+    }
+}
+mma()
 
+
+// Question 5
+function ocSpray() {
+    let ocSpray = prompt('Would you believe that I willingly put peppers into my eyes?').toLocaleLowerCase();
+    if (ocSpray === 'yes' || ocSpray === 'y') {
+        console.log("Good answer!")
+        alert('It felt like the longest 2 hours of my life!');
+    } else {
+        alert('I actually did it 3 times if you can belieive it.')
+    }
+}
+ocSpray()
 
 // Question 6 instructions
 // As a user, I would like to be guided to an answer through a series of feedback responses so that I can learn more about the site owner.
@@ -80,52 +98,62 @@
 // Indicates through an alert if the guess is “too high” or “too low”.
 // It should give the user exactly four opportunities to get the correct answer.
 // After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
-
-
 // Question 6
 // response if:
 // - guess is too high
-
 // - guess is too low
 // - guess is correct
 // - how many attempts they have left
 // - if there are no attempts left
 // - if there are no attempts left, give them the answer
-
 // let attemptsRemaining = 4
 // let userHasAnsweredCorrectly = false
 // // console.log("You have only 4 attempts");
 
 
+// Question 6
+let attemptsRemaining = 4;
 
-while (attemptsRemaining) {
+while (attemptsRemaining > 0) {
     attemptsRemaining--;
+console.log 
     let jaredCatAge = parseInt(prompt('Did you know I have a cat? Guess his age'));
-    // console.log('That is incorrect. You have ${attemptsRemaining} attempts remaining.')
-    if (jaredCatAge === 5) {
-        alert('That guess it too high! She might be old but shes not that old.')
+
+    if (jaredCatAge === NaN){
+        alert ('Thats not an age for a cat!');
+        attemptsRemaining++;
     }
-    else if (jaredCatAge === 2) {
-        alert('That guess is too low. She wasnt born yesterday you know!')
+    else if (jaredCatAge > 3){
+        alert('That guess is too high! She might be old but shes not that old.');
     }
-    else if (jaredCatAge === 3) {
+    else if (jaredCatAge < 3){
+        alert('That guess is too low. She wasnt born yesterday you know!');
+    }
+    if (jaredCatAge === 3){
         alert('That is absolutely right!')
+        break;
     }
-    else if (jaredCatAge <= 4) {
-        alert('Could be but im thinking just a tad younger.')
-    }
-    else {
-        alert('The correct answer is 3! She was 2 when we met a year ago.')
+    if (attemptsRemaining === 0){
+        alert('The correct answer is 3! She was 2 when we met a year ago.');
     }
 }
 
+
+// As a user, I would like to guess the answer to a question that could have many possibilities so that I can have fun with with a guessing game.
+// Add a 7th question that has multiple possible correct answers that are stored in an array.
+// Give the user 6 attempts to guess the correct answer.
+// The guesses will end once the user guesses a correct answer or they run out of attempts.
+// Display all the possible correct answers to the user.
+// Consider using a loop of some sort for this question.
+
+// As a user, I would like to know my final score so that I can know how well I did.
+// Keep track of the total number of correct answers. At the end tell them how many they got correct out of the 7 questions asked.
 
 
 
 // Question 7
 let guess = 7
-let score = 0
-arr = []
+while (lives) = []
 
 // let myFavPasta = [Penne, Rigatoni, Linguine, ]
 
